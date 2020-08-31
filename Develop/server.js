f8c12de6907086b624c2375a38f9fa8a92eb0ea6
demi0504 <ripley.demi@gmail.com>
2020-08-31 11:12:09 -0500

    server.js started:

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
