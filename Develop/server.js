99321f1192edf79b3d35b540ca2c40dc0ed70fe9
demi0504 <ripley.demi@gmail.com>
2020-08-31 11:38:24 -0500

    gotta put that port in there

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
@@ -11,5 +13,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
-
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
