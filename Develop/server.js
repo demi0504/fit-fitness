b3bf30951858f59914ee8e2506c53d2d90593707
demi0504 <ripley.demi@gmail.com>
2020-08-31 11:34:54 -0500

    mongoose connection:

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
-const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,3 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
