0e74ec0490fe5726ce0d3b77ad03514163b8ca09
demi0504 <ripley.demi@gmail.com>
2020-09-08 14:35:35 -0500

    html routes added.

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
@@ -14,10 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-app.use(require("./routes/apiRoutes"));
-app.use(require("./routes/htmlRoutes"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
