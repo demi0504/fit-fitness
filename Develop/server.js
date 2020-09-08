f6d867816114a3d693b8ce84c5b4c19a44e251ce
demi0504 <ripley.demi@gmail.com>
2020-09-08 14:13:40 -0500

    added models and routes folders

--- Develop/server.js
+++ Develop/server.js
@@ -4,14 +4,20 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
