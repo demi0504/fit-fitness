e19f6c4cd591b58e62ac1f68cc73d6dda7c2299a
demi0504 <ripley.demi@gmail.com>
2020-09-08 22:22:12 -0500

    added more values to schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,14 +3,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: {
-        type: Array,
-        default: []
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter a duration in minutes"
+        },
+        weight:{
+          type:Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
     }
-});
+  }
+);
 
 module.exports = mongoose.model("Workout", WorkoutSchema);
